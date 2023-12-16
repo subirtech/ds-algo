1580095ffec795a75b6545cd1ecd2b74e6e783cf
sub <sub@softwareag.com>
2023-12-16 09:41:22 +0530

    Validate Sorted Array

---
+++ com/recursion/ValidateSortedArray.java
@@ -0,0 +1,18 @@
+package com.recursion;
+
+public class ValidateSortedArray {
+    public static boolean isSorted(int[] input, int idx)
+    {
+      if(idx==input.length-1)
+          return true;
+      if(input[idx]>input[idx+1])
+          return false;
+
+      return isSorted(input,++idx);
+    }
+
+    public static void main(String[] args) {
+    int[] input=new int[]{1,2,3,6,5,6,7,8,9};
+    System.out.println(isSorted(input,0));
+    }
+}
