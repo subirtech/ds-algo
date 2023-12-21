07b88d161fa3ca5279d7b6ed37b3dfad9509624f
sub <sub@softwareag.com>
2023-12-21 23:33:53 +0530

    Permutation of String characters

---
+++ com/backtracking/StringPermutation.java
@@ -0,0 +1,30 @@
+package com.backtracking;
+
+/**
+ * Permutation of String characters
+ * Complexity=O(n*n!)
+ */
+public class StringPermutation {
+
+    public static void permute(String str, String permStr,int index)
+    {
+       if(str.length()==0)
+       {
+           System.out.println(permStr);
+           return;
+       }
+
+        for(int i=0;i<str.length();i++)
+        {
+            char c=str.charAt(i);
+            String newStr=str.substring(0,i)+str.substring(i+1);
+            permute(newStr,permStr+c,index++);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        permute("ABCD","",0);
+    }
+}
